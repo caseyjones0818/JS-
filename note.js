6c9df4d81d651be60f3b3de15e2a4bb7d9720f83
Javascrpt高级程序设计笔记 <451418817@qq.com>
2018-08-15 10:07:38 +0800

    Update note.js

--- note.js
+++ note.js
@@ -17,5 +17,53 @@ alert(message); // "hi"
 ECMAScript 中有 5 种简单数据类型（也称为基本数据类型）：Undefined、Null、Boolean、Number
 和 String。还有 1种复杂数据类型——Object，Object 本质上是由一组无序的名值对组成的。
 
+# 第4章 变量、作用域和内存问题 
+动态的属性
+var person = new Object();
+person.name = "Nicholas";
+alert(person.name); //"Nicholas" 
 
+
+但是，我们不能给基本类型的值添加属性，尽管这样做不会导致任何错误。比如：
+var name = "Nicholas";
+name.age = 27;
+alert(name.age); //undefined 
+
+复制变量值
+// 1
+var num1 = 5;
+var num2 = num1; 
+// 2
+var obj1 = new Object();
+var obj2 = obj1;
+obj1.name = "Nicholas";
+alert(obj2.name); //"Nicholas" 
+
+传递参数
+// 1
+function addTen(num) {
+ num += 10;
+ return num;
+} 
+// 2
+var count = 20;
+var result = addTen(count);
+alert(count); //20，没有变化
+alert(result); //30 
+// 3
+function setName(obj) {
+ obj.name = "Nicholas";
+}
+var person = new Object();
+setName(person);
+alert(person.name); //"Nicholas" 
+// 4
+function setName(obj) {
+ obj.name = "Nicholas";
+ obj = new Object();
+ obj.name = "Greg";
+}
+var person = new Object();
+setName(person);
+alert(person.name); //"Nicholas" 
 
