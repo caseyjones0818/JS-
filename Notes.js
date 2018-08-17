3a01a67746f343327b69fda4669af228c99f3448
Javascrpt高级程序设计笔记 <451418817@qq.com>
2018-08-17 15:35:53 +0800

    Update Notes.js

--- Notes.js
+++ Notes.js
@@ -542,7 +542,86 @@ getTimezoneOffset() 返回本地时间与UTC时间相差的分钟数。例如，
 RegExp 类型
 // RegExp使用不多不用记录
 
-Function 类型
+*Function 类型
+函数通常是使用函数声明语法定义的，如下面的例子所示。
+// 1
+function sum (num1, num2) {
+ return num1 + num2;
+}
+这与下面使用函数表达式定义函数的方式几乎相差无几。
+// 2
+var sum = function(num1, num2){
+ return num1 + num2;
+}; 
+以上代码定义了变量 sum 并将其初始化为一个函数。有读者可能会注意到，function 关键字后面
+没有函数名。这是因为在使用函数表达式定义函数时，没有必要使用函数名——通过变量 sum 即可以引
+用函数。另外，还要注意函数末尾有一个分号，就像声明其他变量时一样。
+// 3
+var sum = new Function("num1", "num2", "return num1 + num2"); // 不推荐
+
+# 一个函数可能会有多个名字，如下面的例子所示。
+function sum(num1, num2){
+ return num1 + num2;
+}
+alert(sum(10,10)); //20
+var anotherSum = sum;
+alert(anotherSum(10,10)); //20
+sum = null;
+alert(anotherSum(10,10)); //20 
+以上代码首先定义了一个名为 sum()的函数，用于求两个值的和。然后，又声明了变量 anotherSum，
+并将其设置为与 sum 相等（将 sum 的值赋给 anotherSum）。注意，使用不带圆括号的函数名是访问函
+数指针，而非调用函数。此时，anotherSum 和 sum 就都指向了同一个函数，因此 anotherSum()也
+可以被调用并返回结果。即使将 sum 设置为 null，让它与函数“断绝关系”，但仍然可以正常调用
+anotherSum()。
+
+# 没有重载（深入理解）
+将函数名想象为指针，也有助于理解为什么 ECMAScript 中没有函数重载的概念。以下是曾在第 3
+章使用过的例子。
+function addSomeNumber(num){
+ return num + 100;
+}
+function addSomeNumber(num) {
+ return num + 200;
+}
+显然，这个例子中声明了两个同名函数，而结果则是后面的函数覆盖了前面的函数。以上代码实际
+上与下面的代码没有什么区别。
+var addSomeNumber = function (num){
+ return num + 100;
+};
+addSomeNumber = function (num) {
+ return num + 200;
+};
+var result = addSomeNumber(100); //300
+通过观察重写之后的代码，很容易看清楚到底是怎么回事儿——在创建第二个函数时，实际上覆盖
+了引用第一个函数的变量 addSomeNumber
+
+ *函数声明和函数表达式区别
+ #解析器在向执行环境中加载数据时，对函数声明和函数表达式并非一视同仁。
+ #解析器会率先读取函数声明，并使其在执行任何代码之前可用（可以访问）；至于函数表达式，则必须等到解析器执行到它所在的代码行，
+ #才会真正被解释执行。请看下面的例子。
+ // 1
+ alert(sum(10,10));
+ function sum(num1, num2){
+  return num1 + num2;
+ } 
+以上代码完全可以正常运行。因为在代码开始执行之前，解析器就已经通过一个名为函数声明提升（function declaration hoisting）的过程，
+读取并将函数声明添加到执行环境中。对代码求值时，JavaScript引擎在第一遍会声明函数并将它们放到源代码树的顶部。
+所以，即使声明函数的代码在调用它的代码后面，JavaScript 引擎也能把函数声明提升到顶部。如果像下面例子所示的，把上面的函数声明改为等价
+的函数表达式，就会在执行期间导致错误。
+// 2
+alert(sum(10,10));
+var sum = function(num1, num2){
+ return num1 + num2;
+}; 
+以上代码之所以会在运行期间产生错误，原因在于函数位于一个初始化语句中，而不是一个函数声明。
+换句话说，在执行到函数所在的语句之前，变量 sum 中不会保存有对函数的引用；而且，由于第一行代码就会导致“unexpected identifier”（意外标识符）错误，
+实际上也不会执行到下一行。除了什么时候可以通过变量访问函数这一点区别之外，函数声明与函数表达式的语法其实是等价的。
+#也可以同时使用函数声明和函数表达式，例如 var sum = function sum(){}。不过，这种语法在 Safari 中会导致错误。
+
+*作为值的函数
+
+
+
 
 
 
